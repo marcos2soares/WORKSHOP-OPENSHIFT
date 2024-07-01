f0dc6c3fe5e107cca10cabbbc08681d25f60aa98
Marcos Soares <marcos2.soares@gmail.com>
2024-07-01 11:59:16 -0300

    Labs

--- app_build/cria_servico_rota.sh
+++
@@ -1,6 +0,0 @@
-
-oc expose deployment <deployment name>
-
- oc expose deployment appvt121170-deploy
-
-oc create route edge --service=appvt121170-deploy --insecure-policy=Redirect
--- app_build/import_image.sh
+++
@@ -1,5 +0,0 @@
-
-oc import-image ubi9 --from=registry.redhat.io/ubi9 --confirm
-
-
-oc get is
